83f05b2156e4d215415d6ca11c7315daa0046943
Thomas MORAS <moras@PEGASUS.local>
2018-07-09 14:47:53 +0200

    Initial commit

---
+++ routes/web.php
@@ -0,0 +1,28 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+//$this->app['router']->get('/', function() { return 'Coucou'; });
+//app('router')->get('/', function() { return 'Coucou'; });
+
+Route::get('{n}', function($n) {
+    return 'Je suis la page ' . $n . ' !';
+});
+
+Route::get('/home', ['as' => 'home', function()
+{
+    return 'Je suis la page d\'accueil !';
+}]);
+
