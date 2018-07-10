6dd196deeb0b40fc11bf98a6723040aa39c5b900
nicoooo42 <nicolas.lamanna@wanadoo.fr>
2018-07-10 12:32:58 +0200

    sds

--- resources/views/profil.blade.php
+++ resources/views/profil.blade.php
@@ -6,28 +6,26 @@
 <div class="container">
     <form method="POST" action="{!! url('profil') !!}" accept-charset="UTF-8">
         {!! csrf_field() !!}
-        <label for="nom">Entrez votre nom : </label>
-        <input name="nom" type="text" id="nom">
 
-        <br>
-
-        <label for="prenom">Entrez votre prenom : </label>
-        <input name="prenom" type="text" id="prenom">
-
-        <br>
-
-        <label for="departement">Entrez votre departement : </label>
-        <input name="departement" type="number" id="departement">
-
-        <br>
-
-        <label for="email">Entrez votre email : </label>
-        <input name="email" type="email" id="email">
-
-
-
-
-        <input type="submit" value="Envoyer !">
+        <form>
+            <div class="form-group">
+                <label for="nom">Entrez votre nom : </label>
+                <input name="nom" type="text" class="form-control" id="nom">
+            </div>
+            <div class="form-group">
+                <label for="prenom">Entrez votre prenom : </label>
+                <input name="prenom" type="text" class="form-control" id="prenom">
+            </div>
+            <div class="form-group">
+                <label for="departement">Entrez votre departement : </label>
+                <input name="departement" type="number" class="form-control" id="departement">
+            </div>
+            <div class="form-group">
+                <label for="email">Entrez votre email : </label>
+                <input name="email" type="email" class="form-control" id="email">
+            </div>
+            <button type="submit" class="btn btn-primary">Submit</button>
+        </form>
     </form>
 </div>
 @endsection
