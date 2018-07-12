d15b1ec805e242d00bd3f42b2763c40c921d80bc
Thomas Moras <moras.thomas@gmail.com>
2018-07-12 15:43:07 +0200

    Auto stash before merge of "master" and "origin/master"

--- app/Http/Controllers/ProfilController.php
+++ app/Http/Controllers/ProfilController.php
@@ -17,33 +17,41 @@ class ProfilController extends Controller
     public function index()
     {
         $user = auth()->user();
-        $domaines = Domaine::all();
-        $contrats = Contrat::all();
+        $domaines = Domaine::where([['id', '<>',  $user->domaine_id],['id', '<>',  1]])
+            ->get();
+        $contrats = Contrat::where([['id', '<>',  $user->contrat_id],['id', '<>',  1]])
+            ->get();
         return view('profil',['utilisateur' => $user, 'domaines' => $domaines, 'contrats' => $contrats]);
     }
 
     public function create(Request $request)
     {
-
-        $this->validate($request, [
-            'image_file' => 'required|image|mimes:jpeg,png,jpg,gif,svg|max:2048',
-        ]);
-
-        $imageName = time().'.'.$request->image_file->getClientOriginalExtension();
-        $request->image_file->move(public_path('images'), $imageName);
-
         $user = auth()->user();
+
+        if($request->input('image_file') != null) {
+            $this->validate($request, [
+                'image_file' => 'required|image|mimes:jpeg,png,jpg,gif,svg|max:2048',
+            ]);
+            $imageName = time().'.'.$request->image_file->getClientOriginalExtension();
+            $request->image_file->move(public_path('images'), $imageName);
+            $user->image = $imageName;
+        }
+
         $user->name = $request->input('name');
         $user->prenom = $request->input('prenom');
         $user->description = $request->input('description');
         $user->departement = $request->input('departement');
-        $user->image = $imageName;
+        $user->email = $request->input('email');
 
-        $domaine = Domaine::find($request->input('domaine'));
-        $user->domaine_id = $domaine->id;
+        if($request->input('domaine') != null) {
+            $domaine = Domaine::find($request->input('domaine'));
+            $user->domaine_id = $domaine->id;
+        }
 
-        $contrat = Contrat::find($request->input('contrat'));
-        $user->contrat_id = $contrat->id;
+        if($request->input('contrat') != null) {
+            $contrat = Contrat::find($request->input('contrat'));
+            $user->contrat_id = $contrat->id;
+        }
 
         $user->save();
         return redirect()->route('profil');
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -3,14 +3,13 @@
 @section('content')
 <div class="container">
     <div class="row">
-        <div class="col-md-8 col-md-offset-2">
+        <div class="col-md-8 col-md-offset-4">
             <div class="panel panel-default">
                 <div class="panel-heading">Register</div>
 
                 <div class="panel-body">
-                    <form class="form-horizontal" method="POST" action="{{ route('register') }}">
+                    <form class="form-horizontal box" method="POST" action="{{ route('register') }}">
                         {{ csrf_field() }}
-
                         <div class="form-group{{ $errors->has('name') ? ' has-error' : '' }}">
                             <label for="name" class="col-md-4 control-label">Name</label>
 
